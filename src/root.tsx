a253099c8aab20158b205ed72cba25e076ff5e06
caculadoranutricional <calculadoranutricional@gmail.com>
2026-02-20 23:08:48 -0300

    feat: perfect static configuration

---
+++ src/root.tsx
@@ -0,0 +1,17 @@
+import { component$ } from "@builder.io/qwik";
+import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from "@builder.io/qwik-city";
+
+export default component$(() => {
+  return (
+    <QwikCityProvider>
+      <head>
+        <meta charSet="utf-8" />
+        <title>Qwik Static Adapter</title>
+      </head>
+      <body lang="en">
+        <RouterOutlet />
+        <ServiceWorkerRegister />
+      </body>
+    </QwikCityProvider>
+  );
+});
